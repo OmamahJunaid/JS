61344827262ffa069aed9b59455f994062376f56
omamah junaid <junaidomamah@gmail.com>
2023-10-06 16:15:25 +0500

    add all

---
+++ app.js
@@ -0,0 +1,111 @@
+// // chapter 6
+// // Q1
+// var x = x++
+// var x = ++x
+// // Q2
+// var x = 100
+// x--
+// console.log(x)
+// // Q3
+// var x = 50;
+// var y = x++;
+// console.log(y) //50
+// // Q4
+// var y = 50;
+// var z = --y;
+// console.log(z)  // 49 
+// // Q5
+// var x = ++1
+// var newNum =x
+// // Q6
+// var x = ++1
+// var newNum =x
+// // Q7
+// var num = 10
+// num++
+// var newNum= num
+// alert(newNum)
+// // chapter 7
+// // Q1
+// var calculatedNum = 2 + (2 * 6);
+// console.log(calculatedNum) //14
+// // Q2
+// var calculatedNum = (2 + 2) * 6;
+// console.log(calculatedNum) //24
+// // Q3
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum)//24
+// // Q4
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum )//18
+// Q5
+var calculatedNum= (2 + 2) *(4 + 10)
+console.log(calculatedNum)
+
+// Q6
+ var calculatedNum= 2 + (2 * 4 )+ 10
+ console.log(calculatedNum)
+// Q7
+var calculatedNum= 4 / 2 * 4
+console.log(calculatedNum)
+// chapter 8
+// Q1
+var num = "2"+"2"
+console.log(num) //22
+// Q2
+var msg = ("hello"+"dolly")
+alert (msg)
+
+// Q3
+alert("33" + 3) //333
+// Q4
+var name1 = "pakistan"
+var name2 = "zindabad"
+alert (name1+name2)
+// Q5
+var num =2
+var str = "name"
+console.log(str.concat(num)
+)
+// Q6
+var name1 =["omamah" ,"junaid"]
+var res = name1.join()
+console.log(res)
+// chapter 9
+// Q1
+var firstName = prompt("enter first name")
+console.log(firstName)
+// Q3
+var yourName = prompt("Enter Your Name");
+// Q4
+var firstName = prompt("enter first name")
+console.log(typeof firstName)
+console.log(firstName)
+// Q5
+
+// Q6
+// chapter 10
+// Q1
+var city = "Karachi"
+if (city = "Karachi") {
+console.log("The City OF Lights")}
+// Q2
+var x = 10
+var y = +prompt("enter value if y")
+if (x===y){
+  var z =  prompt("enter value of x")
+  console.log(z)
+ }
+// Q3
+var zipCode =+prompt("enter zipcode")
+if(zipCode===10010){
+    alert("karachi")
+}
+else(alert("Please write correct city"))
+// Q4
+var num = +prompt("enter value")
+if(num===3){
+    num= num+1
+}
+console.log(num)
+
